baa6188ecbbf75440d2445c6a766144047bdac79
Thinh Nguyen <nqt900@gmail.com>
2025-11-07 15:25:19 +0700

    update prob

--- 4-get-vnm-probability.py
+++ 4-get-vnm-probability.py
@@ -40,54 +40,44 @@ def read_multiple_csv_files(directory_path='./', pattern='*.csv'):
 # Usage
 combined_data = read_multiple_csv_files('data/vn', '*.csv')
 q1 = combined_data[(combined_data['gadm_name'] == 'Quận 1') & (combined_data['ds'] == '2025-10-01')]
-print("Q1:", q1["home_to_ping_distance_category"], q1["distance_category_ping_fraction"])
+# print("Q1:", q1["home_to_ping_distance_category"], q1["distance_category_ping_fraction"])
 q3 = combined_data[(combined_data['gadm_name'] == 'Quận 3') & (combined_data['ds'] == '2025-10-01')]
-print("Q3:", q3["home_to_ping_distance_category"], q3["distance_category_ping_fraction"])
+# print("Q3:", q3["home_to_ping_distance_category"], q3["distance_category_ping_fraction"])
 q5 = combined_data[(combined_data['gadm_name'] == 'Quận 5') & (combined_data['ds'] == '2025-10-01')]
-print("Q5:", q5["home_to_ping_distance_category"], q5["distance_category_ping_fraction"])
+# print("Q5:", q5["home_to_ping_distance_category"], q5["distance_category_ping_fraction"])
 q7 = combined_data[(combined_data['gadm_name'] == 'Quận 7') & (combined_data['ds'] == '2025-10-01')]
-print("Q7:", q7["home_to_ping_distance_category"], q7["distance_category_ping_fraction"])
+# print("Q7:", q7["home_to_ping_distance_category"], q7["distance_category_ping_fraction"])
 q10 = combined_data[(combined_data['gadm_name'] == 'Quận 10') & (combined_data['ds'] == '2025-10-01')]
-print("Q10:", q10["home_to_ping_distance_category"], q10["distance_category_ping_fraction"])
+# print("Q10:", q10["home_to_ping_distance_category"], q10["distance_category_ping_fraction"])
+q4 = combined_data[(combined_data['gadm_name'] == 'Quận 4') & (combined_data['ds'] == '2025-10-01')]
+# print("Q4:", q4["home_to_ping_distance_category"], q4["distance_category_ping_fraction"])
 
-probabilities = [
-    {
-        "place_name": "District 1",
-        "0": 0.338331,
-        "(0, 10)": 0.619545,
-        "[10, 100)": 0.034696,
-        "100+": 0.007459
-    },
-    {
-        "place_name": "District 3",
-        "0": 0.356897,
-        "(0, 10)": 0.614933,
-        "[10, 100)": 0.023094,
-        "100+": 0.005119
-    },
-    {
-        "place_name": "District 5",
-        "0": 0.368877,
-        "(0, 10)": 0.598244,
-        "[10, 100)": 0.026583,
-        "100+": 0.006394
-    },
-    {
-        "place_name": "District 7",
-        "0": 0.351673,
-        "(0, 10)": 0.613202,
-        "[10, 100)": 0.032559,
-        "100+": 0.002576
-    },
-    {
-        "place_name": "District 10",
-        "0": 0.356323,
-        "(0, 10)": 0.616555,
-        "[10, 100)": 0.035387,
-        "100+": 0.023637
+def get_correct_index(place_name, q):
+    ob = {
+        "place_name": place_name
     }
+    for i in range(len(q["home_to_ping_distance_category"])):
+        print(q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,1])
+        if q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,0] == "0":
+            ob["0"]= float(q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,1])
+        elif q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,0] == "(0, 10)":
+            ob["(0, 10)"]= float(q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,1])
+        elif q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,0] == "[10, 100)":
+            ob["[10, 100)"]= float(q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,1])
+        elif q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,0] == "100+":
+            ob["100+"]= float(q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,1])
+        else:
+            ob["unknown"]= float(q[["home_to_ping_distance_category", "distance_category_ping_fraction"]].iat[i,1])
+    return ob
 
-]
+probabilities = []
+probabilities.append(get_correct_index("District 1", q1))
+probabilities.append(get_correct_index("District 3", q3))
+probabilities.append(get_correct_index("District 4", q4))
+probabilities.append(get_correct_index("District 5", q5))
+probabilities.append(get_correct_index("District 7", q7))
+probabilities.append(get_correct_index("District 10", q10))
+print(probabilities)
+
 with open("./probability.json", 'w') as f:
     json.dump(probabilities, f, indent=4) # 'indent=4' for pretty-printing
-
